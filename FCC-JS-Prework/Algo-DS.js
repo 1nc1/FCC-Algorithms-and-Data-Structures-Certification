88145eea342741019003c66ec3409dfbb887e29e
1nc1 <1nc1github@gmail.com>
2019-07-31 21:57:41 -0700

    Working on counting cards function, not done

--- FCC-JS-Prework/Algo-DS.js
+++ FCC-JS-Prework/Algo-DS.js
@@ -5,4 +5,17 @@
 //     return removed;
 // };
 
-// ------------------
+// ------------------------------------------
+
+// let count = "";
+//   switch(card) {
+//     case 2:
+//       count += 1;
+//       break;
+//     case 3:
+//       count += 1;
+//       break;
+//     case 4:
+//       count += 1;
+
+// not finished with above algorithm
