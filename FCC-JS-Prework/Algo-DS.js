60a0b3a6cf4c15c839a5f5262d83138458ddab39
1nc1 <1nc1github@gmail.com>
2019-08-02 23:44:44 -0700

    i think if else statements advance the time complexity compared to a switch, maybe, maybe not

--- FCC-JS-Prework/Algo-DS.js
+++ FCC-JS-Prework/Algo-DS.js
@@ -1,21 +1,21 @@
-// const nextInLine = (arr, item) => {
-//     arr.push(item);
-//     var removed = arr.shift();
+const nextInLine = (arr, item) => {
+    arr.push(item);
+    var removed = arr.shift();
 
-//     return removed;
-// };
+    return removed;
+};
 
 // ------------------------------------------
 
-// let count = "";
-//   switch(card) {
-//     case 2:
-//       count += 1;
-//       break;
-//     case 3:
-//       count += 1;
-//       break;
-//     case 4:
-//       count += 1;
+let count = "";
+  switch(card) {
+    case 2:
+      count += 1;
+      break;
+    case 3:
+      count += 1;
+      break;
+    case 4:
+      count += 1;
 
 // not finished with above algorithm
