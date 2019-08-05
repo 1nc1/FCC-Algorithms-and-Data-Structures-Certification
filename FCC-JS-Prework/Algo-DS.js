1a8b48df2e4791e7d8301517717ec718e58644f9
1nc1 <1nc1github@gmail.com>
2019-08-04 23:25:37 -0700

    Will continue to work on algo tmr

--- FCC-JS-Prework/Algo-DS.js
+++ FCC-JS-Prework/Algo-DS.js
@@ -1,27 +1,43 @@
-const nextInLine = (arr, item) => {
-    arr.push(item);
-    var removed = arr.shift();
+// const nextInLine = (arr, item) => {
+//     arr.push(item);
+//     var removed = arr.shift();
 
-    return removed;
-};
+//     return removed;
+// };
 
 // ------------------------------------------
 
-let count = "";
-  switch(card) {
-    case 2:
-      count += 1;
-      break;
-    case 3:
-      count += 1;
-      break;
-    case 4:
-      count += 1;
+var count = 0;
+
+function cc(card) {
+    // Only change code below this line
+    switch(card){
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+        count++;
+        break;
+      case 10:
+      case "J":
+      case "Q":
+      case "K":
+      case "A":
+        count--;
+        break;
+    }
+    if (count > 0){
+      return count + " Bet";
+    } else {
+      return count + " Hold";
+    }
+    // Only change code above this line
   }
 // not finished with above algorithm
 
-function sayHello(name) {
-    console.log("Hello" + name);
-}
+// function sayHello(name) {
+//     console.log("Hello" + name);
+// }
 
 // added bracket colors for better scope recognizability
