47f77de8f3e9a0a8401b3b946b5bf7c173466c35
1nc1 <1nc1github@gmail.com>
2019-08-09 23:21:04 -0700

    Algo that takes given arr values, squares them, returns same arr

--- FCC-JS-Prework/Algo-DS.js
+++ FCC-JS-Prework/Algo-DS.js
@@ -165,3 +165,15 @@
 
 // console.log(greaterY([1,3,5,7],3));
 // --------------------------------------------------------------------------------------------------------------
+// Given an array with multiple values, write a function
+// that replaces each value in the array with the product
+// of the original value multiplied by itself.
+
+function squareVal(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        arr[i] *= arr[i];
+    }
+    return arr;
+}
+
+console.log(squareVal([1,5,10,2]));
